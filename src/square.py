63d099c3db6446c1acc972d3e30d8b57ff92690c
getta15getta15 <134844296+getta15getta15@users.noreply.github.com>
2023-06-19 01:07:02 +0300

    Update square.py

--- src/square.py
+++ src/square.py
@@ -4,7 +4,7 @@ class Square(Figure):
 
     def __init__(self, name, side):
         super().init(self, name)
-        if not isinstance(side, int) or side > 0:
+        if not isinstance(side, int) or side <= 0:
             raise ValueError(f'Can not create square with side {side}')
         self.side = side
         self.area = self.get_area()
