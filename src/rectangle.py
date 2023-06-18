9eef97c5d37038f1a9f791bed7d35477e4fb8dd2
getta15getta15 <134844296+getta15getta15@users.noreply.github.com>
2023-06-19 01:27:53 +0300

    Update rectangle.py

--- src/rectangle.py
+++ src/rectangle.py
@@ -3,7 +3,7 @@ from figure import Figure
 class Rectangle(Figure):
 
     def __init__(self, name, side_a, side_b):
-        super().init(self, name)
+        super().__init__(name)
         if not isinstance(side_a, int) or side_a <= 0:
             raise ValueError(f'Can not create rectangle with side {side_a}')
         if not isinstance(side_b, int) or side_b <= 0:
