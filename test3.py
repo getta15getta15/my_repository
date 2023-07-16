e1ff44453a8618d6aabbacad48dfa7bd4c798b76
getta15getta15 <134844296+getta15getta15@users.noreply.github.com>
2023-07-16 15:47:26 +0300

    Create test3.py

---
+++ test3.py
@@ -0,0 +1,31 @@
+import requests
+import pytest
+
+@pytest.mark.parametrize("url", ["https://jsonplaceholder.typicode.com/"])
+def test_itswork(url):
+    my_rqst = requests.get(f'{url}')
+    assert my_rqst.status_code == 200
+
+@pytest.mark.parametrize("url", ["https://jsonplaceholder.typicode.com/posts/"])
+@pytest.mark.parametrize("post", ["1"])
+def test_get_post(url, post):
+    my_rqst = requests.get(f'{url}{post}')
+    assert str(my_rqst.json()["id"]) == post
+
+@pytest.mark.parametrize("url", ["https://jsonplaceholder.typicode.com/comments?postId="])
+@pytest.mark.parametrize("post", ["1"])    
+def test_get_comment(url, post):
+    my_rqst = requests.get(f'{url}{post}')
+    assert len(my_rqst.json()) != 0
+
+@pytest.mark.parametrize("url", ["https://jsonplaceholder.typicode.com/posts?userId="])
+@pytest.mark.parametrize("user", ["1"])    
+def test_get_by_user(url, user):
+    my_rqst = requests.get(f'{url}{user}')
+    assert len(my_rqst.json()) != 0
+
+@pytest.mark.parametrize("url", ["https://jsonplaceholder.typicode.com/posts"])
+@pytest.mark.parametrize("param", ["{'title': 'foo','body': 'bar','userId': '938'}"])    
+def test_create_post(url, param):
+    my_rqst = requests.post(url, data=param)
+    assert len(my_rqst.json()) != 0
