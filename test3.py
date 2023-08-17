2c949683561dac2d76460b23ab56131e4ea19209
getta15getta15 <134844296+getta15getta15@users.noreply.github.com>
2023-08-17 18:45:58 +0300

    Update test3.py

--- test3.py
+++ test3.py
@@ -16,16 +16,16 @@ def test_get_post(url, post):
 @pytest.mark.parametrize("post", ["1"])    
 def test_get_comment(url, post):
     my_rqst = requests.get(f'{url}{post}')
-    assert len(my_rqst.json()) != 0
+    assert my_rqst.status_code == 200 and len(my_rqst.json()) != 0
 
 @pytest.mark.parametrize("url", ["https://jsonplaceholder.typicode.com/posts?userId="])
 @pytest.mark.parametrize("user", ["1"])    
 def test_get_by_user(url, user):
     my_rqst = requests.get(f'{url}{user}')
-    assert len(my_rqst.json()) != 0
+    assert my_rqst.status_code == 200 and len(my_rqst.json()) != 0
 
 @pytest.mark.parametrize("url", ["https://jsonplaceholder.typicode.com/posts"])
 @pytest.mark.parametrize("param", ["{'title': 'foo','body': 'bar','userId': '938'}"])    
 def test_create_post(url, param):
     my_rqst = requests.post(url, data=param)
-    assert len(my_rqst.json()) != 0
+    assert my_rqst.status_code == 200 and len(my_rqst.json()) != 0
