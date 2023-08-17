a8f546fc49e861d289df71ff2877c0b34e2f6d6e
getta15getta15 <134844296+getta15getta15@users.noreply.github.com>
2023-08-17 16:14:25 +0300

    Update test_1.py

--- test_1.py
+++ test_1.py
@@ -7,33 +7,36 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def test_1(get_browser, get_url):
-    if get_browser == "chrome":
-        browser = webdriver.Chrome()
-    elif get_browser == "ie":
-        browser = webdriver.Ie()
-    elif get_browser == "firefox":
-        browser = webdriver.Firefox()
-    browser.get(get_url)
-    browser.close()
-    assert 1 == 1
+    try:
+        if get_browser == "chrome":
+            browser = webdriver.Chrome()
+        elif get_browser == "ie":
+            browser = webdriver.Ie()
+        elif get_browser == "firefox":
+           browser = webdriver.Firefox()
+        browser.get(get_url)
+        browser.close()
+        assert True
+    except:
+        assert False
     
 def test_main_1():
     browser = webdriver.Chrome()
     browser.get('https://www.opencart.com/')
     try:
         element = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, "support")))
-        assert 1 == 1
+        assert True
     except:
-        assert 1 == 0
+        assert False
 
 def test_main_2():
     browser = webdriver.Chrome()
     browser.get('https://www.opencart.com/')
     try:
         element = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, "newsletter")))
-        assert 1 == 1
+        assert True
     except:
-        assert 1 == 0
+        assert False
 
 def test_admin_1():
     browser = webdriver.Chrome()
@@ -41,9 +44,9 @@ def test_admin_1():
     try:
         element = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, "input-username")))
         element.send_keys('admin')
-        assert 1 == 1
+        assert True
     except:
-        assert 1 == 0
+        assert False
                 
 
 def test_market_1():
@@ -52,9 +55,9 @@ def test_market_1():
     try:
         element = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, "input-sort")))
         element.click()
-        assert 1 == 1
+        assert True
     except:
-        assert 1 == 0
+        assert False
         
 def test_register_1():
     browser = webdriver.Chrome()
@@ -64,7 +67,7 @@ def test_register_1():
         element.send_keys('admin')
         element = WebDriverWait(browser, 30).until(EC.presence_of_element_located((By.ID, "button-register")))
         element.click()
-        assert 1 == 1
+        assert True
     except:
-        assert 1 == 0
+        assert False
         
