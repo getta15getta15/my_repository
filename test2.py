675209f39a77a2c0ea0729a26331b8d3e8ccb287
getta15getta15 <134844296+getta15getta15@users.noreply.github.com>
2023-08-17 16:34:36 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -9,12 +9,16 @@ def test_itswork(url):
 @pytest.mark.parametrize("url_paaram", ["not_find_data"])
 def test_not_find_data(url_paaram):
     my_rqst = requests.get("https://api.openbrewerydb.org/v1/breweries/autocomplete?query={url_paaram}")
-    assert len(my_rqst.json()) == 0
+    assert my_rqst.status_code == 200 and len(my_rqst.json()) == 0
 
 def test_sub_reqst():
     my_rqst = requests.get("https://api.openbrewerydb.org/v1/breweries/random")
     my_rqst = requests.get(f'https://api.openbrewerydb.org/v1/breweries/' + my_rqst.json()[0]['id'])
-    assert len(my_rqst.json()) != 0
+    if my_rqst.status_code == 200:
+        my_rqst = requests.get(f'https://api.openbrewerydb.org/v1/breweries/' + my_rqst.json()[0]['id'])
+        assert len(my_rqst.json()) != 0
+    else:
+        assert False
 
 @pytest.mark.parametrize("url, status_code", [("https://www.openbrewerydb.org/errrrrrror", 404)])
 def test_error_url(url, status_code):
