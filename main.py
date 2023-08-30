e34af6cabd4580005336194a65cd365c3c6f8ea6
getta15getta15 <134844296+getta15getta15@users.noreply.github.com>
2023-08-30 23:39:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,10 @@ from _datetime import date
 #output = subprocess.check_output(['ps', 'aux']).decode('utf-8')
 
 # пришлось генерировать строку
-output = r"USER       PID %CPU %MEM    VSZ   RSS TTY      STAT START   TIME COMMAND"
-output = output + chr(13) + r"root         1  300.3  0.0   4608  3648 pts/0    Ss   06:15   0:00 bash" 
-output = output + chr(13) + r"root         7  100  0.7   8536  4256 pts/0    R+   06:15   0:00 ps aux"
-output = output + chr(13) + r"test         8  101  0.1   8536  4256 pts/0    R+   06:15   0:00 ps aux"
+output =                    r"USER       PID %CPU     %MEM        VSZ   RSS TTY      STAT START   TIME COMMAND"
+output = output + chr(13) + r"root         1  25      0.4       4608  3648 pts/0    Ss   06:15   0:00 bash" 
+output = output + chr(13) + r"root         7  20      0.2       8536  4256 pts/0    R+   06:15   0:00 ps aux"
+output = output + chr(13) + r"test         8  15      0.7       8536  4256 pts/0    R+   06:15   0:00 ps aux"
  
 lines = output.splitlines()
 
@@ -42,8 +42,8 @@ for line in lines:
     cpu = cpu + float(parts[2])
     mem = mem + float(parts[3])
 
-    arr_cpu.update({parts[1]: arr_cpu.get(parts[1], 0) + float(parts[2])})
-    arr_mem.update({parts[1]: arr_mem.get(parts[1], 0) + float(parts[3])})
+    arr_cpu.update({parts[10]: arr_cpu.get(parts[1], 0) + float(parts[2])})
+    arr_mem.update({parts[10]: arr_mem.get(parts[1], 0) + float(parts[3])})
     
 report = report + chr(13) + 'Пользователи системы:'
 for i, users_elmnt in enumerate(users):
@@ -55,14 +55,15 @@ report = report + chr(13) + 'Пользовательских процессов
 for i, users_elmnt in enumerate(users):
     report = report + chr(13) + users_elmnt + "   " + str(users.get(users_elmnt))
 
-report = report + chr(13) + 'Всего CPU используется %:' + "  " + str(cpu)
+report = report + chr(13) + 'Всего CPU используется %:' + "  " + str("{:10.2f}".format(cpu))
 
-print('Всего памяти используется %:' + "  " + str(mem))
+report = report + chr(13) + 'Всего памяти используется %:' + "  " + str("{:10.2f}".format(mem))
 
 top_cpu = dict(sorted(arr_cpu.items(), key=lambda x: x[1], reverse = True)[:1])
 top_mem = dict(sorted(arr_mem.items(), key=lambda x: x[1], reverse = True)[:1])
-report = report + chr(13) + 'Больше всего памяти использует:' + "  " + str(top_cpu)
-report = report + chr(13) + 'Больше всего CPU использует:' + "  " + str(top_mem)
+
+report = report + chr(13) + 'Больше всего памяти использует:' + "  " + ([key for key in top_cpu.keys()][0])[:20]
+report = report + chr(13) + 'Больше всего CPU использует:' + "  " + ([key for key in top_mem.keys()][0])[:20]
 
 print(report)
 
